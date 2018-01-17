74005e39dd8aaee30b93d1a203a5a5cb07edc1c3
Richard Bartels <richard.t.bartels@gmail.com>
2018-01-17 17:59:43 +0100

    added MC sampler

--- Distances.py
+++ Distances.py
@@ -13,7 +13,7 @@ def ymw16(DM, l, b):
 
     Requires an installed version of the ymw16 model in the local
     directory.
-    
+
     A base error on the measured distance is 20% as well as the mean
     variation in distance calculated from the neighbouring pixels.
 
@@ -22,23 +22,17 @@ def ymw16(DM, l, b):
     DM [array] : Dispersion measure # in pp/cm^3
     l [array] : longitude (galactic coordinates)
     b [array] : latitude (galactic coordinates)
-    
+
     Returns
     -------
-    Dist [array] : Distance to MSP # in pc
+    Dist [array] : Distance to MSP # in kpc
     """
-    # try:
-    #     p = subprocess.check_output(["./ymw16", "Gal", str(l), str(b),str(DM), "1"])
-    # except subprocess.CalledProcessError as e:
-    #     if e.returncode == 53 or e.returncode == 137:
-    #         p = e.output
-    #     else:
-    #         raise e
-    # Dist = float(p[105:114])/1e3
     Dist = ymw.distances(l=l, b=b, DM=DM)
     return Dist
 
-def dist_pdf(name, DM, l, b, n_dbins, MC=True, save_files=False, plots=False):
+def dist_pdf(name, DM, l, b, n_dbins,
+    MC=True, save_files=False, plots=False,
+    n_grid=10, n_MC=1000):
     """
     Calcultes the distance to a source using the provided
     despersion measure. This function assumes the ymw16 model
@@ -46,7 +40,7 @@ def dist_pdf(name, DM, l, b, n_dbins, MC=True, save_files=False, plots=False):
 
     Requires an installed version of the ymw16 model in the local
     directory.
-    
+
     The pdf is calculated by varying the parameters in the ymw16 model
     and recalculating the distance to the MSP which is the biggest source
     of systematics in the distance calculation
@@ -57,7 +51,9 @@ def dist_pdf(name, DM, l, b, n_dbins, MC=True, save_files=False, plots=False):
     DM [array] : Dispersion measure # in pp/cm^3
     l [array] : longitude (galactic coordinates)
     b [array] : latitude (galactic coordinates)
-    
+    n_grid [int] : number of grid points for each variable
+    n_MC [float] : number of monte carlo samples
+
     Keyword arguments:
     MC -- Monte Carlo vs grid scan (default True)
     save_files -- saves pdfs as txt files (default False)
@@ -65,48 +61,121 @@ def dist_pdf(name, DM, l, b, n_dbins, MC=True, save_files=False, plots=False):
 
     Returns
     -------
-    Dist [array, array] : PDFs of the distance to MSPs # in pc
+    Dist [array, array] : PDFs of the distance to MSPs # in kpc
     """
-    filename = 'ymw16_v1.3/ymw16par.txt'
-    names = np.loadtxt(filename, usecols=(0,), dtype='str')
-    vals = np.loadtxt(filename, usecols=(1,))
+    # original best fit values
+    filename_bf = 'ymw16_v1.3/ymw16par_bestfit.txt'
+    names_bf = np.loadtxt(filename_bf, usecols=(0,), dtype='str')
+    vals_bf = np.loadtxt(filename_bf, usecols=(1,))
 
-    n1_list = np.linspace(0.008, 0.016, 18)
-    H1_list = np.linspace(1200, 2000, 18)
-    dist_pdfs = []
+    # Load the input file for the ymw16_v1.3 code
+    filename_input = 'ymw16_v1.3/ymw16par.txt'
+    names = np.loadtxt(filename_input, usecols=(0,), dtype='str')
+    vals = np.loadtxt(filename_input, usecols=(1,))
+    if (names==names_bf).all():
+        vals = vals_bf
+        print "Resetting input file... Done!"
+    else:
+        print "Cannot reset input file, quitting"
+        quit()
 
+    # Load the file containing parameter ranges and best fit values
+    filename_ranges = 'ymw16_v1.3/ymw16par_ranges.txt'
+    names_ranges = np.loadtxt(filename_ranges, usecols=(0,), dtype='str')
+    val_lo, val_hi, val_bf, val_unc = np.loadtxt(
+        filename_ranges, usecols=(1,2,3,4)).T
+
+    # Create list
     D_list = []
-    for n1 in n1_list:
-        vals[names=='n1'] = n1
-        for H1 in H1_list:
-            vals[names=='H1'] = H1
-            np.savetxt(filename, zip(names, vals), 
-                delimiter=" ", fmt="%s") 
+    dist_pdfs = []
+    if MC:
+        tag="_MC"
+
+        # Mask that says which parameters to vary
+        mask1 = (val_lo != val_hi)
+        print "Number of free parameters: %i"%mask1.sum()
+
+        # Draw from a uniform distribution
+        val_MC = np.random.uniform(low=val_lo[mask1], high=val_hi[mask1],
+            size=(n_MC, mask1.sum()))
+
+        # Match the MC values to input file values
+        ind_order = np.array([(n==names).argmax() for n in names_ranges[mask1]])
+
+        # Run MC
+        for i in range(n_MC):
+            # update values
+            vals[ind_order] = val_MC[i] # Set values
+            np.savetxt(filename_input, zip(names, vals),
+                delimiter=" ", fmt="%s")
             D_list.append(ymw16(DM=DM, l=l, b=b))
-    D_list = np.array(D_list)
+        D_list = np.array(D_list)
+
+
+    else: # Perform a grid scan
+        tag="_grid"
+        n1_list = np.linspace(0.008, 0.016, n_grid)
+        H1_list = np.linspace(1200, 2000, n_grid)
+        # ngn_list = np.linspace(1, 3, 3)
+        # Wgn_list = np.linspace(10, 20, 3)
+        # Agn_list = np.linspace(120, 130, 3)
+        # nLI_list = np.linspace(0, 3, 4)
+        nlb1_list = np.linspace(0.5, 1.5, 3)
+        nlb2_list = np.linspace(1, 3, 3)
+
+        for n1 in n1_list:
+            vals[names=='n1'] = n1
+            for H1 in H1_list:
+                vals[names=='H1'] = H1
+                # for ngn in ngn_list:
+                    # vals[names=='ngn'] = ngn
+                # for Wgn in Wgn_list:
+                #     vals[names=='Wgn'] = Wgn
+                # for Agn in Agn_list:
+                #     vals[names=='Agn'] = Agn
+                # for nLI in nLI_list:
+                #     vals[names=='nLI'] = nLI
+                for nlb1 in nlb1_list:
+                    vals[names=='nlb1'] = nlb1
+                    for nlb2 in nlb2_list:
+                        vals[names=='nlb2'] = nlb2
+                        np.savetxt(filename_input, zip(names, vals),
+                            delimiter=" ", fmt="%s")
+                        D_list.append(ymw16(DM=DM, l=l, b=b))
+        D_list = np.array(D_list)
+
+    # --> Make histograms
     for i in range(DM.size):
         d_hist = np.histogram(D_list[:,i], bins=n_dbins, density=True)
         dist_pdfs.append(d_hist)
 
+    # --> make plots
     if plots:
         for i in range(DM.size):
             plt.figure()
             plt.hist(D_list[:,i], bins=n_dbins, normed=True)
             plt.ylabel('N')
-            plt.title('%s_test.pdf'%(str(name[i])))
+            plt.title('%s'%(str(name[i])))
             plt.xlabel('D [kpc]')
-            plt.savefig('%s_test.pdf'%(str(name[i])))
+            plt.savefig('plots/%s%s.pdf'%(name[i], tag))
             plt.close()
 
+    # --> save files
     if save_files:
         for i in range(DM.size):
-            np.savetxt(name[i], zip(dist_pdfs[i][0], dist_pdfs[i][1]), 
-                    delimiter=" ", fmt="%s") 
+            np.savetxt("output/%s%s.dat"%(name[i], tag),
+                zip(dist_pdfs[i][0], dist_pdfs[i][1]),
+                    delimiter=" ")
 
     return dist_pdfs
 
-names = np.array(["B0628-28", "B1237+25", "B1237+201"])
-DM = np.array([4., 2., 3.])
-l = np.array([10., 20., 30.])
-b = np.array([15., 25., 35.])
-newthing = dist_pdf(names, DM, l, b, 20, plots=True, save_files=True)
+names = np.array(["B0628-28", "B1237+25"])
+DM = np.array([34.36, 9.2755])
+l = np.array([236.95, 252.44])
+b = np.array([-16.75, 86.54])
+# newthing = dist_pdf(names, DM, l, b,
+#     n_dbins=40, n_grid=181, n_MC=10000,
+#     plots=True, save_files=True)
+newthing = dist_pdf(names, DM, l, b,
+    n_dbins=40, n_grid=40,
+    plots=True, save_files=True, MC=False)
--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # MSPDist
-Calculates probability density functions for the distances to Millisecond Pulsars
+Calculates probability density functions for the distances to Millisecond Pulsars.
+Code consists of a wrapper for the ymw16 electron density model (Yao, Manchester and Wang, 2017, Astrophys. J., 835, 29; arXiv:1610.09448). It has been adopted to scan over the parameters of the electron density model.
 
-## Requirements
+## Requirements and installation
 - numpy
 - cython
 
+Compile the cython code by running ./make.sh. Then py_ymw16 can be imported by python.
+
 ## Errors
 We assume the ymw16 model for the electron density distribution of our galaxy
-To calculate the errors on the distances to the pulsars we assume that the 
+To calculate the errors on the distances to the pulsars we assume that the
 parameters of the ymw16 are uncertain and performa grid scan to produce an array
 of distances to each MSP. We then create a pdf through a normalised histogram of this list.
+
+## Changes in the original ymw16 code.
+The version of the ymw16 code provided contains some changes compared to the original in the following files:
+- cn.h
+- dmdtau.c: now returns a double and print statements have been turned off
+- ymw16par.txt
+
+The original best fit parameters from the ymw16 paper can be found in ymw16par_bestfit.txt.
